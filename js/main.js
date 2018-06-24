5f681b148184ca5f7d7104a2809228c4a72fb2d1
Abhishta Gatya <abhishtagatya@yahoo.com>
2018-06-24 14:29:02 +0700

    Create JSON File data/distrolist and added some more distros

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-var distro = distros[Math.floor(Math.random()*distros.length)];
-
-console.log(distro)
 function WriteData(name, link, desc){
 	document.getElementById("a1").href = link;
 	document.getElementById("a1").innerHTML = name;
 	document.getElementById("info").innerHTML = desc;
 }
 
-WriteData(distro[0], distro[1], distro[2]);
+$.ajax({
+	url: 'data/distrolist.json',
+	dataType: 'json',
+	type: 'get',
+	cache: false,
+	success: function(data) {
+			var distros = data.distros
+			var distro = distros[Math.floor(Math.random()*distros.length)]
+
+			// Writes to the HTML
+			WriteData(distro.name, distro.link, distro.desc);
+		}
+})
